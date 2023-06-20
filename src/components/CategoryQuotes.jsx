10bb9f81736e94f6eb3488f8a68134fd20ecddeb
ThusharaGit <thushara.weeraparakrama@gmail.com>
2023-06-20 15:33:32 +0200

    preview the card with only quote stage completed

--- src/components/CategoryQuotes.jsx
+++ src/components/CategoryQuotes.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
+import Quote from './Quote'
+import { useState } from 'react'
 
 function CategoryQuotes({ quotes, setQuotes }) {
+
+    const [wish, setWish] = useState("")
+
     return (
         <div>
             <div className="cardQuotesDiv">
-               
-                    <div className='cardPrev'></div>
-             
+                <div className='prevBox'>
+
+                    <h2  className="card-title">Preview</h2>
+
+                    <div className='cardPrev'>
+                        <p style={{fontWeight:'bold',fontStyle:'italic'}}>{wish}</p>
+                    </div>
+
+                </div>
+
                 <div className="quotesDivBody">
                     <h2 className="card-title">Select The Quote</h2>
                     {
-                        quotes.map(quote => <p className='quote'>{quote}</p>)
+                        quotes.map(quote => <Quote quote={quote} setWish={setWish} />)
                     }
 
                     <div className="card-actions justify-end">
-                        <button onClick={() =>
-                        {
+                        <button onClick={() => {
                             setQuotes("")
-                        }} className="btn btn-primary">Back</button>
+                        }} className="btn btn-secondary">Back</button>
                     </div>
                 </div>
             </div>
---
+++ src/components/Quote.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Quote({quote, setWish}) {
+  return (
+    <div>
+        <button onClick={() => 
+        {
+            setWish(quote)
+        }
+        } className='quote'>{quote}</button>
+    </div>
+  )
+}
+
+export default Quote
